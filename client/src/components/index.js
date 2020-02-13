26db3a99ea07a8d9042cab3e5288b753574a5e12
Jamie Everett <jreverett2442@gmail.com>
2020-02-13 17:12:50 +0000

    Improve project structure

---
+++ client/src/components/index.js
@@ -0,0 +1,11 @@
+/*
+e.g:
+    import ComponentA from './ComponentA/ComponentA';
+    import ComponentB from './ComponentB/ComponentB';
+    
+    export { ComponentA, ComponentB };
+*/
+
+import App from './App/App';
+
+export { App };
