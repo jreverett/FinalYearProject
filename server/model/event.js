e43404984c34acd5735b6a86fdb0225c011e7091
Jamie Everett <jreverett2442@gmail.com>
2020-02-19 17:07:05 +0000

    Add user and event schemas

---
+++ server/model/event.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+// create user schema
+const EventSchema = mongoose.Schema({
+  owner: {
+    type: String,
+    required: true
+  },
+  title: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  start: {
+    type: Date,
+    required: true
+  },
+  end: {
+    type: Date
+  },
+  cost: {
+    type: Number,
+    required: true
+  },
+  images: {
+    type: Array
+  }
+});
+
+module.exports = Event = mongoose.model('Event', EventSchema);
---
+++ server/model/user.js
@@ -0,0 +1,63 @@
+const mongoose = require('mongoose');
+var crypto = require('crypto');
+
+// create user schema
+const UserSchema = mongoose.Schema({
+  type: {
+    type: Number,
+    required: true
+  },
+  firstname: {
+    type: String,
+    required: true
+  },
+  lastname: {
+    type: String,
+    required: truea
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  address: {
+    type: String
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  salt: {
+    type: String,
+    required: true
+  },
+  verified: {
+    type: Boolean,
+    required: true
+  },
+  events: {
+    type: Array
+  }
+});
+
+// Generate a password w/ salt
+UserSchema.methods.setPassword = function(password) {
+  this.salt = crypto.randomBytes(16).toString('hex');
+
+  // password w/ a length of 64
+  // 1000 hash iterations
+  this.hash = crypto
+    .pbkdf2Sync(password, this.salt, 1000, 64, 'sha512')
+    .toString('hex');
+};
+
+// Check if the password is valid (using password payload and database salt)
+// Hash the request and compare it to the stored value
+UserSchema.methods.passwordIsValid = function(password) {
+  var hash = crypto
+    .pbkdf2Sync(password, this.salt, 1000, 64, 'sha512')
+    .toString('hex');
+  return this.hash == hash;
+};
+
+module.exports = User = mongoose.model('User', UserSchema);
