e7f4ec8e68be65f16d57da0b67543c79e92fd9eb
Jamie Everett <jreverett2442@gmail.com>
2020-02-20 14:59:52 +0000

    Create API with test route, link it to the client

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -4,6 +4,21 @@ import { NavBar } from '../index';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { apiResponse: '' };
+  }
+
+  callAPI() {
+    fetch('http://localhost:9000/testAPI')
+      .then(res => res.text())
+      .then(res => this.setState({ apiResponse: res }));
+  }
+
+  componentWillMount() {
+    this.callAPI();
+  }
+
   render() {
     return (
       <Fragment>
@@ -28,6 +43,8 @@ class App extends Component {
             </Route>
           </Switch>
         </Router>
+
+        <p>{this.state.apiResponse}</p>
       </Fragment>
     );
   }
--- server/server.js
+++ server/server.js
@@ -1,7 +1,49 @@
+const createError = require('http-errors');
 const express = require('express');
+const path = require('path');
+const mongoose = require('mongoose');
+const cors = require('cors');
 require('dotenv').config();
 
+const testAPIRouter = require('./api/routes/testAPI');
+
+//////////////////////////////////////////////////////////////
+// SERVER SETUP
 const app = express();
 const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// routing
+app.use('/testAPI', testAPIRouter);
+
+// catch 404's and send them to the error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // only show error in dev mode
+  res.locals.message = err.message;
+  res.locals.error = process.env.NODE_ENV === 'development' ? err : {};
+
+  // show error page
+  res.status(err.status || 500);
+  res.json({ message: err.message, error: err });
+});
+
+//////////////////////////////////////////////////////////////
+// MONGODB
+var db = mongoose.connection;
+const mongoString = process.env.MONGODB_URL;
+
+// setup listeners
+db.on('error', console.error.bind(console, 'connection error: '));
+db.once('open', function() {
+  console.log('[mongodb] connection established with MongoDB');
+});
 
 app.listen(port, () => console.log(`listening on port ${port}`));
