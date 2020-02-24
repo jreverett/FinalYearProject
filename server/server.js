5f888501e47606e9531fe7485dd6ad67dd7096e7
Jamie Everett <jreverett2442@gmail.com>
2020-02-24 23:36:29 +0000

    removed redundant line

--- server/server.js
+++ server/server.js
@@ -23,7 +23,6 @@ app.use('/api', userRouter);
 // heroku + react router bug fix
 app.get('*', (req, res) => {
   let url = path.join(__dirname, '../client/build', 'index.html');
-  if (!url.startsWith('/app/')) url = url.substring(1);
   res.sendFile(url);
 });
 
