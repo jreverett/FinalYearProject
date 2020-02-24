0041de5b9fed04317d7847703716ffc0c597d2ac
Jamie Everett <jreverett2442@gmail.com>
2020-02-24 23:18:43 +0000

    Fix ordering of signup parameters

--- client/src/services/user.js
+++ client/src/services/user.js
@@ -1,10 +1,10 @@
 import { API_URL } from '../config';
 
-function signup(firstname, lastname, email, password, address) {
+function signup(firstname, lastname, email, address, password) {
   const requestOptions = {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ firstname, lastname, email, password, address })
+    body: JSON.stringify({ firstname, lastname, email, address, password })
   };
 
   return fetch(`${API_URL}/api/user/signup`, requestOptions)
