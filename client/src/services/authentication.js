0c3972096e3ccd88d7cbc0a8e1b3279eb17ef581
Jamie Everett <jreverett2442@gmail.com>
2020-02-29 13:47:44 +0000

    Add localStorage getting/setting for login/out

--- client/src/services/authentication.js
+++ client/src/services/authentication.js
@@ -1,5 +1,12 @@
+import { BehaviorSubject } from 'rxjs';
+
 import { API_URL } from '../config';
 import { handleResponse } from '../utilities';
+
+// setup user observable
+const currentUserSubject = new BehaviorSubject(
+  JSON.parse(localStorage.getItem('loggedInUser'))
+);
 
 function login(email, password) {
   const requestOptions = {
@@ -11,19 +18,25 @@ function login(email, password) {
   return fetch(`${API_URL}/api/user/login`, requestOptions)
     .then(handleResponse)
     .then(user => {
-      if (user) {
-        // TODO
-      }
+      // store user info in localStorage, push user data to observable
+      localStorage.setItem('loggedInUser', JSON.stringify(user));
+      currentUserSubject.next(user);
 
       return user;
     });
 }
 
 function logout(email) {
-  // TODO: this (delete jwt session token)
+  // remove user value from localStorage, push null to observable
+  localStorage.removeItem('loggedInUser');
+  currentUserSubject.next(null);
 }
 
 export const authenticationService = {
   login,
-  logout
+  logout,
+  loggedInUser: currentUserSubject.asObservable(),
+  get loggedInUserValue() {
+    return this.currentUserSubject.value;
+  }
 };
