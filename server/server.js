e75050e567767d72cd7aee6d49b5e20e0d628b84
Jamie Everett <jreverett2442@gmail.com>
2020-03-30 22:39:49 +0100

    Increase payload size limit from 100KB to 50MB

--- server/server.js
+++ server/server.js
@@ -15,7 +15,8 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '50mb' })); // Increase the payload limit to allow for multiple image uploads
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(express.static(path.join(__dirname, '../client/build')));
 
 // routing
